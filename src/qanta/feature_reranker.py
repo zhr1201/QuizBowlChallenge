60ef009f6d3b9c4429ccf62fd7dff4396dbe6fa2
Haoran Zhou <zzhhrr001@126.com>
2021-04-14 01:51:06 -0400

    change dict build logic

--- src/qanta/feature_reranker.py
+++ src/qanta/feature_reranker.py
@@ -14,10 +14,14 @@ import wikipedia
 import logging
 from tqdm import tqdm
 import pickle
+import os.path
+import urllib.request
+import json
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(20)
+WIKI_DUMP_URL = "https://s3-us-west-2.amazonaws.com/pinafore-us-west-2/qanta-jmlr-datasets/wikipedia/wiki_lookup.json"
 
 
 class FeatureReranker(AbsReranker):
@@ -37,23 +41,41 @@ class FeatureReranker(AbsReranker):
         self,
         path: str,
         retriever: AbsRetriever,
+        wiki_dump: str,
     ):
         '''
         download all the wikipage info in the training set
         Args:
             path: str, pkl model path
             retriever: AbsRetriever, not used here
+            wiki_dump: str, the wiki dump json file path at the time those page lables are created
         '''
+
+        if not os.path.isfile(wiki_dump):
+            logger.warning("Wiki dump doesn't exit, download a new one")
+            urllib.request.urlretrieve(WIKI_DUMP_URL, wiki_dump)
+        with open(wiki_dump, 'r') as f:
+            old_wiki_dict = json.load(f)
+        
         dataset = QuizBowlDataset(guesser_train=True)
         training_data = dataset.training_data()
         answers = training_data[1]
         logger.info("Start extracing wiki pages")
 
+        # get it from the wikipedia API since it has anchor text information
+        # only using page search will have disambuigation issues since page names are changed
+
         for ans in tqdm(answers):
             try:
-                self.wiki_page_dict[ans] = wikipedia.page(ans, auto_suggest=False)
+                wiki_pageid = old_wiki_dict[ans]['id']
+                self.wiki_page_dict[ans] = wikipedia.page(pageid=wiki_pageid)
             except:
-                logger.warning("Fail to get " + ans)
+                logger.warning("Fail to get wikipage %s using the id of the old wikidump " % ans)
+                try:
+                    logger.warning("Using direct page search %s "  % ans)
+                    self.wiki_page_dict[ans] = wikipedia.page(ans, auto_suggest=False)
+                except:
+                    logger.warning("Fail to get " + ans)
 
         with open(path, 'wb') as f:
             pickle.dump({

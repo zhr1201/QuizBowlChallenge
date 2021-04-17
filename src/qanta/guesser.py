0308c589c02671a49b6a75eff4d38fa24224d55c
Haoran Zhou <zzhhrr001@126.com>
2021-04-16 21:34:50 -0400

    Fix a bug

--- src/qanta/guesser.py
+++ src/qanta/guesser.py
@@ -36,7 +36,7 @@ class Guesser(ABC):
             max_n_guesses of titles of the wiki passages (a batch)
         '''
         top_k_list = self.retriever.retrieve(questions)
-        if self.retriever.output_size > max_n_guesses:
+        if self.retriever.output_size < max_n_guesses:
             raise RuntimeError(
                 "Can't produce max_n_guesses cause the retirever doesn't provide enough results")
         if self.reranker is not None:
